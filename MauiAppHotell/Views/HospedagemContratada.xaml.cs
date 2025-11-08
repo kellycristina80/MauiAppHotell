8d6ee6c2e607684dcce280f067a835f7840ce0ff
Kelly Rodrigues <kellycristina80@yahoo.com.br>
2025-11-08 11:10:13 -0300

    Tentando interligar os arquivos para calculo da diaria

--- MauiAppHotell/Views/HospedagemContratada.xaml.cs
+++ MauiAppHotell/Views/HospedagemContratada.xaml.cs
@@ -1,10 +1,21 @@
+using MauiAppHotell.Models;
+
 namespace MauiAppHotell.Views;
 
 public partial class HospedagemContratada : ContentPage
 {
-	public HospedagemContratada()
+    private object labelResumo;
+
+    public HospedagemContratada (Quartos reserva)
 	{
 		InitializeComponent();
+        double total = reserva.ValorDiariaAdulto +
+                      reserva.ValorDiariaCrianca;
+
+        labelResumo.Text = $@"Quarto: {reserva.Descricao}
+Valor da diária adulto: R$ {reserva.ValorDiariaAdulto}
+Valor da diária criança: R$ {reserva.ValorDiariaCrianca}
+Valor total: R$ {total}";
 	}
 
     private void Button_Clicked(object sender, EventArgs e)
@@ -12,7 +23,6 @@ public partial class HospedagemContratada : ContentPage
 		try
 		{
 			Navigation.PopAsync();
-
 		}
 		catch (Exception ex)
 		{

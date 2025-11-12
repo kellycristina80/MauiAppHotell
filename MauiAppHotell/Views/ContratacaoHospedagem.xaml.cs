ad0d240ad907bbe64c15497540ca2a02d79a8f29
Kelly Rodrigues <kellycristina80@yahoo.com.br>
2025-11-12 06:42:20 -0300

    Finalizaçao do projeto agenda 14

--- MauiAppHotell/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotell/Views/ContratacaoHospedagem.xaml.cs
@@ -20,16 +20,27 @@ namespace MauiAppHotell.Views
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            try {
-                Navigation.PushAsync(new HospedagemContratada());
-            }catch (Exception ex)
+            var hospedagem = new Models.Hospedagem()
+            {
+                QntAdultos = (int)stp_adultos.Value,
+                DataCheckIn = dtpck_checkin.Date,
+                DataCheckOut = dtpck_checkout.Date,
+                QntCriancas = (int)stp_criancas.Value,
+                QuartoSelecionado = (Models.Quartos)pck_quarto.SelectedItem
+            };
+            try
+            {
+                Navigation.PushAsync(new HospedagemContratada() { BindingContext=hospedagem});
+            }
+            catch (Exception ex)
             {
                 DisplayAlert("OPS!", ex.Message, "OK");
             }
         }
         private void Sobre_Clicked(object sender, EventArgs e)
         {
-            try { 
+            try
+            {
                 Navigation.PushAsync(new SobreDesenvolvimento());
             }
             catch (Exception ex)
@@ -38,15 +49,15 @@ namespace MauiAppHotell.Views
             }
         }
 
-        
+
         private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
         {
             DatePicker elemento = sender as DatePicker;
             DateTime data_selecionada_checkin = elemento.Date;
             dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
             dtpck_checkout.MaximumDate = data_selecionada_checkin.AddSeconds(6);
-            
-            
+
+
         }
     }
 }
